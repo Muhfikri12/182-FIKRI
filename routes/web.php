25507394cf616c04d7d7ae396db98e16ba590540
User <fikrisacila0@gmail.com>
2024-05-12 11:17:45 +0700

    Menambahkan view login dan sidebar

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('main.login');
+});
+
+Route::get('/Dashboard', function () {
+    return view('sideBar');
+});
+
